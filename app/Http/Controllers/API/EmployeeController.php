f4efcca2ca56f25a488a3e20121ecdfb0659c649
ahmadnasir123 <nasirahmad240502@gmail.com>
2023-10-08 16:40:49 +0700

    Develop API Authentication

---
+++ app/Http/Controllers/API/EmployeeController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+
+class EmployeeController extends Controller
+{
+    public function fetch(Request $request)
+    {
+        $id = $request->input('id');
+        $nip = $request->input('nip');
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $gender = $request->input('gender');
+        $age = $request->input('age');
+        $phone = $request->input('phone');
+        $user_id = $request->input('user_id');
+        $position_id = $request->input('position_id');
+        $course_id = $request->input('course_id');
+        $kelas_id = $request->input('kelas_id');
+        $limit = $request->input('limit', 10);
+
+        //siak.com/api/employee?id=1
+        $employeeQuery = Employee::query();
+
+        // Get single data
+        if ($id) {
+            $employee = $employeeQuery->with(['user', 'position', 'course', 'kelas' ])->find($id);
+
+            if ($employee) {
+                return ResponseFormatter::success($employee, 'Employee found');
+            }
+
+            return ResponseFormatter::error('Employee not found', 404);
+        }
+
+
+        //siak.com/api/employee
+        // Get multiple data
+        $employees = $employeeQuery;
+
+        if ($name) {
+            $employees->where('name', 'like', '%' . $name . '%');
+        }
+        if ($email) {
+            $employees->where('email', $email);
+        }
+        if ($gender) {
+            $employees->where('gender', $gender);
+        }
+        if ($age) {
+            $employees->where('age', $age);
+        }
+        if ($phone) {
+            $employees->where('phone', 'like', '%' . $phone . '%');
+        }
+        if ($user_id) {
+            $employees->where('user_id', $user_id);
+        }
+        if ($position_id) {
+            $employees->where('position_id', $position_id);
+        }
+        if ($course_id) {
+            $employees->whereHas('team', function ($query) use ($course_id) {
+                $query->where('course_id', $course_id);
+            });
+        }
+
+        return ResponseFormatter::success(
+            $employees->paginate($limit),
+            'Employees Success'
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Auth API
+Route::name('auth.')->group(function () {
+    Route::post('login', [UserController::class, 'login'])->name('login');
+    Route::post('register', [UserController::class, 'register'])->name('register');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [UserController::class, 'logout'])->name('logout');
+        Route::get('user', [UserController::class, 'fetch'])->name('fetch');
+    });
 });
